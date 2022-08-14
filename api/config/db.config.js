4a1c763112767de4f5f8f8338c1578d9240b85fb
seangreen <seanngreen@gmail.com>
2022-08-13 17:29:18 -0700

    New Dockerfile for dev builds without bundling

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -4,7 +4,7 @@ const pg = require('pg');
 
 const connect = () => {
 
-    /*const hostName = process.env.HOST;
+    const hostName = process.env.HOST;
     const userName = process.env.USER;
     const password = process.env.PASSWORD;
     const database = process.env.DB;
@@ -20,13 +20,13 @@ const connect = () => {
             acquire: 20000,
             idle: 5000
         }
-    });*/
-
-    console.log('process.env.PG_CONNECTION_STR ', process.env.PG_CONNECTION_STR)
-
-    const sequelize = new Sequelize(process.env.PG_CONNECTION_STR, {
-        dialectModule: pg
     });
+
+    // console.log('process.env.PG_CONNECTION_STR ', process.env.PG_CONNECTION_STR)
+
+    // const sequelize = new Sequelize(process.env.PG_CONNECTION_STR, {
+    //     dialectModule: pg
+    // });
 
     const db = {};
     db.Sequelize = Sequelize;
